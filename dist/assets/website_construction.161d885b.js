fdb54d82ec0d541482d5e69195ec7c54a106a12a
yosecc <yosec.cervino@gmail.com>
2022-01-17 11:38:39 -0300

    Act

---
+++ dist/assets/website_construction.161d885b.js
@@ -0,0 +1,1 @@
+var s="/assets/website_construction.524b8d92.svg";export{s as _};
--- src/pages/settings/reports/index.vue
+++ src/pages/settings/reports/index.vue
@@ -5,9 +5,9 @@ import { pageTitle } from '/@src/state/sidebarLayoutState'
 // import { useRoute, useRouter } from 'vue-router'
 // import { Api } from '/@src/services'
 import { getCompany, company } from '/@src/pages/companies/companies.ts'
-pageTitle.value = 'Settings'
+pageTitle.value = 'Reports'
 useHead({
-  title: 'Settings',
+  title: 'Reports',
 })
 
 onMounted(()=>{
@@ -21,10 +21,26 @@ onMounted(()=>{
   <SidebarLayout >
     <!-- Content Wrapper -->
     <div class="page-content-inner ">
-
-    
+      <div class="columns is-multiline">
+        <h1 class="title  text-center is-2 column is-12">Page under construction</h1>
+        <div class="column is-4 mx-auto">
+          
+          <img
+            class="website_construction"
+            src="/@src/assets/illustrations/website_construction.svg"
+            alt=""
+          />
+        </div>
+      </div>
+        
        
     </div>
     
   </SidebarLayout>
 </template>
+
+<style lang="scss">
+  .website_construction{
+    width: 100%;
+  }
+</style>
