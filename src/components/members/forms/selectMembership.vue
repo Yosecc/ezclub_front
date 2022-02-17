3d8f6fc38f7eb9f3455aaf23364f4718dcf23fb5
yosecc <yosec.cervino@gmail.com>
2022-02-17 14:27:10 -0300

    sdfds

--- src/components/members/forms/selectMembership.vue
+++ src/components/members/forms/selectMembership.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { computed, ref, reactive, defineProps, defineEmit, watch } from 'vue'
-import { viewInput, setInputValuesData, setInputModelData } from '/@src/models/Mixin.ts'
+import {
+  viewInput,
+  setInputValuesData,
+  setInputModelData,
+} from '/@src/models/Mixin.ts'
 import { getLocationsDiciplines } from '/@src/models/Diciplines.ts'
 import { recurrences } from '/@src/models/Recurrences.ts'
 // import { membershipsData  } from '/@src/models/Members.ts'
@@ -9,34 +13,34 @@ import { discountsData } from '/@src/models/Discounts.ts'
 import { trainers } from '/@src/models/Staffs.ts'
 
 const props = defineProps({
-  type:{
+  type: {
     type: String,
-    default: 'create'
+    default: 'create',
   },
-  inputs:{
+  inputs: {
     type: Array,
-    default: []
+    default: [],
   },
-  title:{
+  title: {
     type: String,
-    default: ''
+    default: '',
   },
-  member:{
+  member: {
     type: Array,
-    default: []
+    default: [],
   },
-  familiares:{
+  familiares: {
     type: Array,
-    default: []
+    default: [],
   },
-  memberships:{
-    type:Array,
-    default: []
-  },
-  notes:{
+  memberships: {
     type: Array,
-    default: []
-  }
+    default: [],
+  },
+  notes: {
+    type: Array,
+    default: [],
+  },
 })
 
 const isLoading = ref(false)
@@ -48,14 +52,14 @@ watch(
   }
 )
 
-const reloadForm = () =>{
-  isLoading.value= true
-  setTimeout(()=>{
-    isLoading.value= false
-  }, 500);
+const reloadForm = () => {
+  isLoading.value = true
+  setTimeout(() => {
+    isLoading.value = false
+  }, 500)
 }
 
-const inputsSteps = computed(()=>{
+const inputsSteps = computed(() => {
   return props.inputs
 })
 
@@ -66,31 +70,31 @@ const membershipsInputs = [
     placeholder: 'Recurrence',
     model: ['recurrence'],
     disabled: false,
-    class: 'is-12'
+    class: 'is-12',
   },
   {
     typeInput: 'selectDataActionChange',
-    name:'memberships_id',
+    name: 'memberships_id',
     placeholder: 'Membership Type',
-    values: [] ,
+    values: [],
     model: '',
     disabled: false,
-    class: 'is-12'
+    class: 'is-12',
   },
   {
     typeInput: 'radioBoxs',
-    name:'recurrences_id',
+    name: 'recurrences_id',
     placeholder: 'Recurrence & Amount',
-    values: [] ,
+    values: [],
     model: '',
     disabled: false,
     class: 'is-12',
     filterName: 'descriptions',
-    otros:'amount'
+    otros: 'amount',
   },
   {
     typeInput: 'hidden',
-    name:'amount',
+    name: 'amount',
     placeholder: 'Amount',
     model: '',
     disabled: false,
@@ -108,11 +112,11 @@ const membershipsInputs = [
   {
     typeInput: 'checkboxGroupSimple',
     name: 'diciplines',
-    text: "Diciplines",
+    text: 'Diciplines',
     model: [],
-    values:[],
+    values: [],
     disabled: false,
-    class: 'is-12'
+    class: 'is-12',
   },
   {
     typeInput: 'number',
@@ -128,7 +132,7 @@ const membershipsInputs = [
     placeholder: 'No initiation fee',
     model: [],
     disabled: false,
-    class: 'is-4'
+    class: 'is-4',
   },
   {
     typeInput: 'selectData',
@@ -147,29 +151,29 @@ const membershipsInputs = [
     model: '',
     disabled: false,
     class: 'is-6',
-    filterOptionText: function(option){
+    filterOptionText: function (option) {
       return `${option.name} ${option.second_name} ${option.last_name}`
-    }
-  }
+    },
+  },
 ]
 
-const copiamembershipsInputs = computed(()=>{
+const copiamembershipsInputs = computed(() => {
   return JSON.parse(JSON.stringify(membershipsInputs))
 })
 
-const inputsFamilies = computed(()=>{
-  let data = []  
-  props.familiares.forEach((element)=>{
-    if(element.find((e)=> e.name == 'name').model != ''){
+const inputsFamilies = computed(() => {
+  let data = []
+  props.familiares.forEach((element) => {
+    if (element.find((e) => e.name == 'name').model != '') {
       // console.log(membershipsInputs)
       let membershipDataFor = JSON.parse(JSON.stringify(membershipsInputs))
-      setInputValuesData(membershipDataFor,'memberships_id', memberships.value)
-      setInputValuesData(membershipDataFor,'discount', discountsData.value)
-      setInputValuesData(membershipDataFor,'staff_id', trainers.value)
-      
-      data.push({ 
+      setInputValuesData(membershipDataFor, 'memberships_id', memberships.value)
+      setInputValuesData(membershipDataFor, 'discount', discountsData.value)
+      setInputValuesData(membershipDataFor, 'staff_id', trainers.value)
+
+      data.push({
         family: element,
-        inputs: membershipDataFor
+        inputs: membershipDataFor,
       })
     }
   })
@@ -178,98 +182,117 @@ const inputsFamilies = computed(()=>{
 })
 
 const change = (val) => {
-  emit('returnData', { memberMembership: inputsSteps, familyMembership: inputsFamilies } )
-  emit('changeStep',val)
+  emit('returnData', {
+    memberMembership: inputsSteps,
+    familyMembership: inputsFamilies,
+  })
+  emit('changeStep', val)
 }
 
 const changeMembership = (obj) => {
-  if(obj.input.name == 'memberships_id'){
-    let membershipSelected = obj.input.values.find((element) => element.id == obj.input.model)
-    setInputModelData(obj.inputsStep, 'initiation_fee', membershipSelected.initiation_fee)
+  if (obj.input.name == 'memberships_id') {
+    let membershipSelected = obj.input.values.find(
+      (element) => element.id == obj.input.model
+    )
+    setInputModelData(
+      obj.inputsStep,
+      'initiation_fee',
+      membershipSelected.initiation_fee
+    )
     let locations = []
-    membershipSelected.locations.forEach((element)=>{
-      if(element.sale == 1){
+    membershipSelected.locations.forEach((element) => {
+      if (element.sale == 1) {
         locations.push(element.company_locations)
       }
     })
     setInputValuesData(obj.inputsStep, 'locations_id', locations)
     let recurrencesData = []
-    membershipSelected.amounts.forEach((element)=>{
-      let recurrencesD = recurrences.value.find((e)=> e.id == element.recurrences_id)
+    membershipSelected.amounts.forEach((element) => {
+      let recurrencesD = recurrences.value.find(
+        (e) => e.id == element.recurrences_id
+      )
       recurrencesD.amount = element.amount
       recurrencesData.push(recurrencesD)
     })
-    setInputValuesData(obj.inputsStep,'recurrences_id', recurrencesData) 
+    setInputValuesData(obj.inputsStep, 'recurrences_id', recurrencesData)
     reloadForm()
   }
-  if(obj.input.name == 'locations_id'){
-    getLocationsDiciplines([obj.input.model]).then((response)=>{
-      setInputValuesData(obj.inputsStep, "diciplines", response.data)
+  if (obj.input.name == 'locations_id') {
+    getLocationsDiciplines([obj.input.model]).then((response) => {
+      setInputValuesData(obj.inputsStep, 'diciplines', response.data)
       reloadForm()
     })
   }
 }
 
 const changeRecurrence = (input, inputs) => {
-  setInputModelData(inputsSteps, 'amount', input.values.find((e)=>e.id == input.model).amount)
+  setInputModelData(
+    inputsSteps,
+    'amount',
+    input.values.find((e) => e.id == input.model).amount
+  )
 }
 
 const changeRecurrenceFamily = (input, inputs) => {
-  setInputModelData(inputs, 'amount', input.values.find((e)=>e.id == input.model).amount)
+  setInputModelData(
+    inputs,
+    'amount',
+    input.values.find((e) => e.id == input.model).amount
+  )
 }
 
-const emit = defineEmit(['changeStep','returnData'])
-
+const emit = defineEmit(['changeStep', 'returnData'])
 </script>
 
 <template>
- <formLayaut
-  :titles="{title: title }"
-  :isLoading="isLoading"
-  :buttons="['next','prev']"
-  :step="4"
-  @changeStep="change"
- >
-
-  <V-Card class="mb-4">
-    <p class=""><b> {{ viewInput(member,'name') }} {{ viewInput(member,'second_name') }} {{ viewInput(member,'last_name') }}</b></p>
-    <p>{{ viewInput(inputsSteps,'diciplines') }}</p>
-    <inputsLayaut
-      :inputs-step="inputsSteps"
-      @changeSelect="changeMembership"
-      @changeRadio="changeRecurrence"
-    />
-  </V-Card>
-
-
-  <V-Card 
-    class="mb-4"
-    v-for="(data, key) in inputsFamilies">
-
-    <p class="mb-5"><b> {{ viewInput(data.family,'name') }} {{ viewInput(data.family,'second_name') }} {{ viewInput(data.family,'last_name') }}</b></p>
-    <!-- <p>{{ viewInput(data.inputs,'diciplines') }}</p> -->
-    <inputsLayaut
-      :inputs-step="data.inputs"
-      @changeSelect="changeMembership"
-      @changeRadio="changeRecurrenceFamily"
-      @changeCheckbox="reloadForm"
-    />
-
-  </V-Card>
-
-  <V-Card
-    class="mb-4"
+  <formLayaut
+    :titles="{ title: title }"
+    :is-loading="isLoading"
+    :buttons="['next', 'prev']"
+    :step="4"
+    @changeStep="change"
   >
-    <inputsLayaut
-      :inputs-step="notes"
-    />
-  </V-Card>
+    <V-Card class="mb-4">
+      <p class="">
+        <b>
+          {{ viewInput(member, 'name') }}
+          {{ viewInput(member, 'second_name') }}
+          {{ viewInput(member, 'last_name') }}</b
+        >
+      </p>
+      <p>{{ viewInput(inputsSteps, 'diciplines') }}</p>
+      <inputsLayaut
+        :inputs-step="inputsSteps"
+        @changeSelect="changeMembership"
+        @changeRadio="changeRecurrence"
+      />
+    </V-Card>
 
- </formLayaut>
+    <V-Card
+      class="mb-4"
+      v-for="(data, key) in inputsFamilies"
+      :key="`da${key}`"
+    >
+      <p class="mb-5">
+        <b>
+          {{ viewInput(data.family, 'name') }}
+          {{ viewInput(data.family, 'second_name') }}
+          {{ viewInput(data.family, 'last_name') }}</b
+        >
+      </p>
+      <!-- <p>{{ viewInput(data.inputs,'diciplines') }}</p> -->
+      <inputsLayaut
+        :inputs-step="data.inputs"
+        @changeSelect="changeMembership"
+        @changeRadio="changeRecurrenceFamily"
+        @changeCheckbox="reloadForm"
+      />
+    </V-Card>
+
+    <V-Card class="mb-4">
+      <inputsLayaut :inputs-step="notes" />
+    </V-Card>
+  </formLayaut>
 </template>
 
-<style lang="scss">
-
-
-
-</style>
+<style lang="scss"></style>
