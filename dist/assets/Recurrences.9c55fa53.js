9132195a692d7c45ea43acdabb6a803bb49f84dd
yosecc <yosec.cervino@gmail.com>
2022-03-16 14:05:59 -0300

    Buld.0.16

--- dist/assets/Recurrences.9c55fa53.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./index.4b786f04.js";import{r as a}from"./vendor.e3a29b40.js";const e=a([]),s=async()=>{const a=await r.get("get_recurrences");return e.value=a.data,a};export{s as g,e as r};
---
+++ src/components/webCam.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref, defineProps, defineEmit } from 'vue'
+</script>
+
+<template>
+  <div>
+    Lorem ipsum, dolor sit, amet consectetur adipisicing elit. A dolor beatae
+    vero harum nobis perferendis quas sequi repudiandae iusto repellat
+    temporibus laboriosam quidem nihil illo, in amet deserunt totam ratione.
+  </div>
+</template>
+
+<style lang="scss"></style>
