a2d39edd041f3c79a29d7be60d1ab3055155ead0
yosecc <yosec.cervino@gmail.com>
2022-04-07 14:35:02 -0300

    Build.1.01

--- dist/assets/Recurrences.4b89d7c2.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index.e55ea7bf.js";import{r}from"./vendor.685e53b0.js";const a=r([]),s=async()=>{const r=await e.get("get_recurrences");return a.value=r.data,r};export{s as g,a as r};
---
+++ dist/assets/servicesPacks.679e988c.js
@@ -0,0 +1,1 @@
+import{A as a}from"./index.a9908f9a.js";import{r as s}from"./vendor.685e53b0.js";const e=s([]),t=async()=>(await a.get("services_packs/get").then((a=>{e.value=a.data})),e);export{t as g,e as s};
