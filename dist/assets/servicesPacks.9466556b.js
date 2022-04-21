2756dbf189731242472ddc961817bbcafca8626f
yosecc <yosec.cervino@gmail.com>
2022-04-21 15:51:09 -0300

    Build 1.25

--- dist/assets/servicesPacks.9466556b.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index.8ac11bf2.js";import{r as s}from"./vendor.6f1ebb59.js";const e=s([]),t=async()=>(await a.get("services_packs/get").then((a=>{e.value=a.data})),e);export{t as g,e as s};
