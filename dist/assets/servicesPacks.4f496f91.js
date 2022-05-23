1c2fadb7d9ae35342b08d84740ad71b57eaee30d
yosecc <yosec.cervino@gmail.com>
2022-05-23 12:12:24 -0300

    Build 1.49 Production

--- dist/assets/servicesPacks.4f496f91.js
+++
@@ -1,1 +0,0 @@
-import{A as s}from"./index.b05546b7.js";import{r as a}from"./vendor.d0c8bc4f.js";const e=a([]),t=async()=>(await s.get("services_packs/get").then((s=>{e.value=s.data})),e);export{t as g,e as s};
