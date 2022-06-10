4c3039b1e9917aa3b1880ebe11ed7aba824f7234
yosecc <yosec.cervino@gmail.com>
2022-06-09 23:47:11 -0300

    BuildInseguro

--- dist/assets/servicesPacks.a99ce83a.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index.de3108be.js";import{r as s}from"./vendor.19da945b.js";const e=s([]),t=async()=>(await a.get("services_packs/get").then((a=>{e.value=a.data})),e);export{t as g,e as s};
