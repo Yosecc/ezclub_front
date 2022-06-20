5ba1b389d918a7cc7c77b7932d4b3e7b9e4c56af
Robert Hernnadez <rahernande96@gmail.com>
2022-06-19 20:44:37 -0400

    add reports filter

--- src/components/setting/reports/reportsTable.vue
+++ src/components/setting/reports/reportsTable.vue
@@ -1,43 +1,65 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue'
-import { reports, getReports } from '/@src/models/Reports.ts'
+import { ref, onMounted } from 'vue'
+import { getReports } from '/@src/models/Reports.ts'
 import moment from 'moment'
+import VLoader from '../../base/loader/V-Loader.vue'
+
+const reports = ref([])
+const loading = ref(false)
 
 onMounted(async () => {
-  await getReports()
+  handleReports()
 })
 
-watch(reports, (to, o) => {
-  console.log(to)
-})
+const handleSearch = async (data: object) => {
+  handleReports(data)
+}
+
+const handleReports = async (data: object = {}) => {
+  loading.value = true
+  try {
+    const response = await getReports(data)
+    reports.value = response.data
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
-  <filterReportsTable />
-  <VCard class="p-">
+  <VCard>
+    <div>
+      <filterReportsTable @search="handleSearch" :loading="loading" />
+    </div>
     <div class="mt-4">
-      <table class="table is-hoverable is-fullwidth">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Description</th>
-            <th scope="col">Date</th>
-            <th scope="col">Type</th>
-            <th scope="col">Amount</th>
-          </tr>
-        </thead>
-        <tbody v-if="reports">
-          <tr v-for="(report, key) in reports" :key="`report-${key}`">
-            <td>{{ report.id }}</td>
-            <td>{{ '' }}</td>
-            <td>
-              {{ moment(report.created_at).format('MM/DD/YYYY') }}
-            </td>
-            <td>{{ '' }}</td>
-            <td>{{ report.amount }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <VLoader class="is-12" center size="small" :active="loading">
+        <table class="table is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Membership</th>
+              <th scope="col">Date</th>
+              <th scope="col">Payment Type</th>
+              <th scope="col">Amount</th>
+            </tr>
+          </thead>
+          <tbody v-if="reports.data && !loading">
+            <tr v-for="(report, key) in reports.data" :key="`report-${key}`">
+              <td>{{ report.id }}</td>
+              <td>{{ `${report.first_name} ${report.last_name}` }}</td>
+              <td>{{ `${report.membership}` }}</td>
+              <td>
+                {{ moment(report.created_at).format('MM/DD/YYYY') }}
+              </td>
+              <td>{{ report.payment_type }}</td>
+              <td>{{ `${report.amount} $` }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </VLoader>
     </div>
   </VCard>
 </template>
--- src/models/Reports.ts
+++ src/models/Reports.ts
@@ -1,17 +1,46 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { Api } from '/@src/services'
 
-export const reportsState = ref([])
-
-export const reports = computed(() => {
-  if (!reportsState.value) {
-    return null
-  }
-  return reportsState.value
-})
-
 export const getReports = async (data: object = {}) => {
-  const response = await Api.get(`reports/memberships`, data)
-  reportsState.value = response.data
+  const response = await Api.get(`reports/memberships`, {
+    params: data,
+  })
   return response
 }
+
+export const filterInsputs = ref([
+  {
+    typeInput: 'select',
+    name: 'report_type',
+    placeholder: 'Report Type',
+    values: ['memberships', 'Orders'],
+    model: '',
+    class: 'is-3',
+    isLabel: true,
+  },
+  {
+    typeInput: 'select',
+    name: 'payment_type',
+    placeholder: 'Payment Type',
+    values: ['Credit Card', 'Cash'],
+    model: '',
+    class: 'is-3',
+    isLabel: true,
+  },
+  {
+    typeInput: 'date',
+    name: 'start_date',
+    placeholder: 'Start Date',
+    model: '',
+    class: 'is-3',
+    isLabel: true,
+  },
+  {
+    typeInput: 'date',
+    name: 'end_date',
+    placeholder: 'End Date',
+    model: '',
+    class: 'is-3',
+    isLabel: true,
+  },
+])
