0fe7acb6554dce116fe24ac94f572fb145d83ba5
yosecc <yosec.cervino@gmail.com>
2022-06-22 22:44:18 -0300

    Build staging 1.70

---
+++ dist/assets/servicesPacks.1197e88e.js
@@ -0,0 +1,1 @@
+import{r as a}from"./vendor.01c5c571.js";import{A as s}from"./index.069ac160.js";const e=a([]),t=async()=>(await s.get("services_packs/get").then((a=>{e.value=a.data})),e);export{t as g,e as s};
