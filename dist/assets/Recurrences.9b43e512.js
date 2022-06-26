e6b6441d87e869c47390601cef9c1c3968eb306a
yosecc <yosec.cervino@gmail.com>
2022-06-26 13:36:09 -0300

    Build Staging 1.70

--- dist/assets/Recurrences.9b43e512.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./vendor.073d5183.js";import{A as a}from"./index.845d558b.js";const e=r([]),s=async()=>{const r=await a.get("get_recurrences");return e.value=r.data,r};export{s as g,e as r};
