38113d4159dc9ec2d85e182100f242e7b87d9cf6
yosecc <yosec.cervino@gmail.com>
2022-09-29 13:28:47 -0300

    Build Staging 1.28

---
+++ dist/assets/V-PlaceloadWrap.f8884120.js
@@ -0,0 +1,1 @@
+import{i as s,j as a,L as e}from"./vendor.435948fe.js";const r={},o={class:"placeload-wrap is-flex"};r.render=function(r,t){return s(),a("div",o,[e(r.$slots,"default")])};export{r as _};
---
+++ dist/assets/sidebar.a6f0cc9b.js
@@ -0,0 +1,1 @@
+import{h as e,i as r,j as o}from"./vendor.435948fe.js";const t={};t.render=function(t,n){const s=e("RouterView");return r(),o(s)};export default t;
