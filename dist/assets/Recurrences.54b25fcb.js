445fa5d8d1c16349ae391309c3bac3f9992111cb
yosecc <yosec.cervino@gmail.com>
2022-10-03 17:39:04 -0300

    1.125-1

---
+++ dist/assets/Recurrences.54b25fcb.js
@@ -0,0 +1,1 @@
+import{A as r}from"./index.3c66f81b.js";import{r as e}from"./vendor.5767c02e.js";const a=e([]),s=async()=>{const e=await r.get("get_recurrences");return a.value=e.data,e};export{s as g,a as r};
