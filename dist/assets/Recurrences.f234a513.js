af053817e2761f682adc1eaab5e7e1d157a8fae1
yosecc <yosec.cervino@gmail.com>
2022-12-27 14:16:40 -0300

    Build Production 1.131 Cambio de dominio

---
+++ dist/assets/Recurrences.f234a513.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index.deccee18.js";import{r}from"./vendor.7d5213a2.js";const a=r([]),s=async()=>{const r=await e.get("get_recurrences");return a.value=r.data,r};export{s as g,a as r};
