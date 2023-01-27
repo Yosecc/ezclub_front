bed6d976ca579a75b6813a61499baba24b446641
yosecc <yosec.cervino@gmail.com>
2023-01-27 17:52:22 -0300

    prueba firma

--- dist/assets/servicesPacks.2c560052.js
+++
@@ -1,1 +0,0 @@
-import{A as s}from"./index.c64878f2.js";import{r as a}from"./vendor.f6529162.js";const e=a([]),t=async()=>(await s.get("services_packs/get").then((s=>{e.value=s.data})),e);export{t as g,e as s};
